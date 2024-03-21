240306f454da20d1fcc1a5aabff32432480846ff
Egor <Egor032324@mail.ru>
2024-03-21 21:21:17 +0300

    refactor: rewrite AppHeader to a functional component

--- src/components/HeaderButton/HeaderButton.jsx
+++ src/components/HeaderButton/HeaderButton.jsx
@@ -1,35 +1,28 @@
-import React from "react";
 import styles from "./HeaderButton.module.css";
 import PropTypes from "prop-types";
 
-export default class HeaderButton extends React.Component {
-  constructor({ link, icon, text, active }) {
-    super();
-    this.link = link;
-    this.icon = icon;
-    this.text = text;
-    this.active = active;
-  }
-  render() {
-    const Icon = this.icon;
-    return (
-      <a href={this.link} className={`${styles.link} pl-5 pr-5 pt-4 pb-4`}>
-        <Icon type={this.active ? "primary" : "secondary"} />
-        <span
-          className={`text text_type_main-default ${
-            this.active ? "text_color_primary" : "text_color_inactive"
-          }`}
-        >
-          {this.text}
-        </span>
-      </a>
-    );
-  }
-}
+const HeaderButton = ({ icon, link, text, active }) => {
+  const Icon = icon;
+
+  return (
+    <a href={link} className={`${styles.link} pt-4 pr-5 pb-4 pl-5`}>
+      <Icon type={active ? "primary" : "secondary"} />
+      <span
+        className={`text text_type_main-default ${
+          active ? "text_color_primary" : "text_color_inactive"
+        }`}
+      >
+        {text}
+      </span>
+    </a>
+  );
+};
 
 HeaderButton.propTypes = {
+  icon: PropTypes.elementType.isRequired,
   link: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
-  icon: PropTypes.elementType.isRequired,
   active: PropTypes.bool,
 };
+
+export default HeaderButton;
