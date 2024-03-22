27476e8552f5dac3e08d721b9fa478643d4262a0
Egor <Egor032324@mail.ru>
2024-03-22 17:59:15 +0300

    feat: make a request for a list of ingredients using the api

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,32 @@
-import "./App.css";
 import AppHeader from "../AppHeader/AppHeader";
 import AppConstructor from "../AppConstructor/AppConstructor";
+import { useState, useEffect } from "react";
 
 function App() {
+  const API_URL = "https://norma.nomoreparties.space/api/ingredients";
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (response.ok) {
+          const data = await response.json();
+          setIngredients(data.data);
+        } else {
+          throw new Error(`Oops! It seems your link ${API_URL} is broken`);
+        }
+      } catch (error) {
+        console.error("Error when executing the request:", error);
+      }
+    };
+
+    fetchIngredients();
+  }, []);
   return (
     <>
       <AppHeader />
-      <AppConstructor />
+      <AppConstructor data={ingredients} />
     </>
   );
 }
